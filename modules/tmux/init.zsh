82a5e80e73f9dd45ae76e251143d6e9376454c91
Robbie Smith <zoqaeski@gmail.com>
2021-12-25 18:49:54 +1100

    Modified tmux plugin to create new sessions.

--- modules/tmux/init.zsh
+++ modules/tmux/init.zsh
@@ -30,20 +30,28 @@ if [[ -z "$TMUX" && -z "$EMACS" && -z "$VIM" && -z "$INSIDE_EMACS" && "$TERM_PRO
   tmux start-server
 
   # Create a 'prezto' session if no session has been defined in tmux.conf.
+  zstyle -s ':prezto:module:tmux:session' name tmux_session || tmux_session='prezto'
   if ! tmux has-session 2> /dev/null; then
-    zstyle -s ':prezto:module:tmux:session' name tmux_session || tmux_session='prezto'
     tmux \
       new-session -d -s "$tmux_session" \; \
       set-option -t "$tmux_session" destroy-unattached off &> /dev/null
   fi
 
-  # Attach to the 'prezto' session or to the last session used. (detach first)
-  exec tmux $_tmux_iterm_integration attach-session -d
+  # Attach to the 'prezto' session if it isn't currently active, otherwise start a new session
+  if ! [[ $(tmux ls -F "#{session_name}: #{?session_attached,attached,not attached}" | grep "$tmux_session: attached") ]]; then
+    exec tmux attach -t "$tmux_session"
+  else
+    exec tmux $_tmux_iterm_integration new-session -c "$PWD"
+  fi
 fi
+
+#
+# Functions
+#
 
 #
 # Aliases
 #
 
-alias tmuxa="tmux $_tmux_iterm_integration new-session -A"
-alias tmuxl='tmux list-sessions'
+# alias tmuxa="tmux $_tmux_iterm_integration new-session -A"
+# alias tmuxl='tmux list-sessions'
