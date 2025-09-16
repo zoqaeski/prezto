a6dc2f359e19475fc30614675c8318f795632e3d
Robbie Smith <zoqaeski@gmail.com>
2025-09-16 17:53:45 +1000

    Added local completion function definitions

--- modules/completion/init.zsh
+++ modules/completion/init.zsh
@@ -12,7 +12,7 @@ if [[ $TERM == dumb ]]; then
 fi
 
 # Add zsh-completions to $fpath.
-fpath=(${0:h}/external/src $fpath)
+fpath=(${0:h}/external/src ${0:h}/functions $fpath)
 
 # Add completion for keg-only brewed curl on macOS when available.
 if (( $+commands[brew] )); then
